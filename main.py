d7550d9cfe0ecac9191b1f649c459890054cf9d8
Dimitri Sobolev <dmsobolev@yahoo.com>
2020-12-12 14:25:22 +0300

    data_stracture_builder method added

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ if __name__ == '__main__':
     walker = Walker('/media/wotori/Media/iPhone11')
     walker.collect_data()
     print(walker.data_bag)
+
+# TODO: просканировать все файлы, получить exif.
+# TODO: на основе exif создать список дат и структуру папок
--- tools/walker.py
+++ tools/walker.py
@@ -1,21 +1,56 @@
 from os import walk
 from os import path
+import time
+from exif import Image
 
 
 class Walker:
-    data_bag = []
 
-    def __init__(self, path):
-        self.task_path = path
+    def __init__(self, media_root_path):
+        self.task_path = media_root_path
+        self.data_bag = []
 
     def collect_data(self):
+        """
+        collecting all data across root catalog
+        """
         for item in walk(self.task_path):
-            if item[2] == []:
+            if not item[2]:
                 continue
             cur_path = item[0]
-            for media_file in item[2]:
-                media_file_path = path.join(cur_path, media_file)
-                self.data_bag.append(media_file_path)
+            for media_file_name in item[2]:
+                media_file_path = path.join(cur_path, media_file_name)
+                self.data_bag.append({"media_root": cur_path,
+                                      'file_name': media_file_name,
+                                      'media_file_path': media_file_path})
 
-            # TODO: просканировать все файлы, получить exif.
-            # TODO: на основе exif создать список дат и структуру папок
+    def data_stracture_builder(self, media_file_name, media_file_path):
+        media_source = media_file_name[:3]
+        media_extention = media_file_name[-3:]
+
+        # strategy for camera photos
+        if media_source == "IMG" and media_extention == "JPG":
+            with open(media_file_path, "rb") as media_file:
+                image = Image(media_file)
+                image_datetime = image.datetime_digitized
+
+        # strategy for camera video
+        elif media_source == "IMG" and media_extention == "MP4":
+            pass
+
+        # strategy for editing AAE file
+        elif media_source == "IMG" and media_extention == "AAE":
+            pass
+
+        # strategy for screenshots
+        elif media_source == "IMG" and media_extention == "PNG":
+            pass
+
+        else:
+            # strategy for pos processed media
+            pass
+
+    def media_builder(self):
+        for file in self.data_bag:
+            with open(file, 'rb'):
+                self.objects_bag.append(Image(file))
